cf89f0756e03fb2d4d6a385d0ec962614074f39e
avery <wuqize5109@qq.com>
2017-07-02 17:19:41 +0800

    第十章 使用webob和werkz处理wsgi

---
+++ chapter10/timeapp_werkz.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+
+import time
+from werkzeug.wrappers import Request, Response
+
+def app(request):
+    host = request.host
+    if ':' in host:
+        host, port = host.split(':', 1)
+    if request.method != 'GET':
+        return Response('501 Not Implemented', status=501)
+    elif host != '127.0.0.1' or request.path != '/':
+        return Response('404 Not Found', status=404)
+    else:
+        return Response(time.ctime())
